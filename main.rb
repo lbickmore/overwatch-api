5cf32a2d0861a9f45b5c53a72ad571deed2c9dc9
Logan Bickmore <lbickmore@users.noreply.github.com>
2016-05-27 19:59:01 -0700

    Temp Update

--- main.rb
+++ main.rb
@@ -6,21 +6,59 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 page = HTTParty.get('https://playoverwatch.com/en-us/career/pc/us/BlueShoesYes-1548')
 doc = Nokogiri::HTML(page)
 
+# Get Catagories
+catagories = []
+doc.xpath('//*[@id="stats-section"]/div/select/option').each do |i|
+  catagories.push(i.text)
+end
+
+data = Hash.new()
+catagories.each do |category|
+  data[category]={}
+end
+
+catagories.each_with_index do |category, i|
+  doc.xpath('//*[@id="stats-section"]/div/div').children[i].xpath('div/div/table/tbody').each do |item|
+    puts item.class
+    item.children.children.children.each_slice(2).to_a.each do |input|
+      data[category][input[0].text] = input[1].text
+    end
+  end
+end
+puts data['Mercy']['Offensive Assists - Average']
+
+
+
+
+
+
+
+
+doc.xpath('//*[@id="stats-section"]/div/div').children[i].xpath('div/div/table/tbody').children.children.children.each_slice(2).to_a
+
+
+catagories.each do |category|
+  doc.xpath('//*[@id="stats-section"]/div/div/div[1]/div/div/table/tbody').each do |item|
+
+# Here you can call data_raw[catagories[n]].inner_html
+
+
+
 all_tables=[]
 doc.xpath('//*[@id="stats-section"]/div/div/div/div/div/table/tbody').each do |item|
   all_tables.push(item.children.children.children.each_slice(2).to_a)
 end
-data={}
+data_raw={}
 all_tables.each do |table|
   table.each do |i|
-    if data[i[0].text]
-      data[i[0].text+' dup'] = i[1].text
+    if data_raw[i[0].text]
+      data_raw[i[0].text+' dup'] = i[1].text
     else
-      data[i[0].text] = i[1].text
+      data_raw[i[0].text] = i[1].text
     end
   end
 end
-data.each do |row|
+data_raw.each do |row|
   puts "#{row[0]},#{row[1].gsub(",",'')}"
 end
 
@@ -30,7 +68,7 @@ end
 #headers_arr = headers_str.gsub(%r{</?[^>]+?>}, '').split("\n")
 
 =begin
-raw_arr = parse_page.css('.data-table').to_a
+raw_arr = parse_page.css('.data_raw-table').to_a
 clean_arr = []
 raw_arr.each do |table|
   table.inner_html.split("\n").each do |row|
@@ -42,7 +80,7 @@ raw_arr.each do |table|
     end
   end
 end
-data_hash = Hash[clean_arr.each_slice(2).to_a]
+data_raw_hash = Hash[clean_arr.each_slice(2).to_a]
 puts clean_arr
 =end
 
