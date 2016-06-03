2b1e51a4cccc4104324359472dc95a71c3858126
lbickmore <loganbickmore91@gmail.com>
2016-06-03 16:21:02 -0700

    Server now partly supported

--- main.rb
+++ main.rb
@@ -5,8 +5,18 @@ require 'redis'
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
+$redis = Redis.new(:url => 'redis://52.32.216.63:6379')
+
 def battle_net_id(id)
-  id = id.strip
+  puts 'Running battle_net_id'
+  #redis = $redis
+  id = id.strip.gsub('-','#')
+
+  # Check redis before checking playoverwatch.com
+  #users=[]
+  #redis.keys.each { |i| users.push(i.split('#')[0])}
+  #if users.include? id
+
   if id.include? '#'
     # Check for PC user
     x = id.split('#')
@@ -35,14 +45,13 @@ def battle_net_id(id)
 end
 
 
-def get_stats(bnet_id)
-  #Usage: ruby script.rb <GamerTag or Battlenet ID>"
+def crawl_stats(bnet_id)
+  puts 'Running crawl_stats'
   if bnet_id == 'No User Found'
-    puts bnet_id
     return bnet_id
   end
-  redis = Redis.new
-  #redis = Redis.new(:url => 'redis://IPHERE')
+
+  redis = $redis
   bnet_arr = bnet_id.split('#')
   if !/\A\d+\z/.match(bnet_arr[1]) #if not a number
     page = HTTParty.get("https://playoverwatch.com/en-us/career/#{bnet_arr[1]}/#{bnet_arr[0]}")
@@ -55,6 +64,8 @@ def get_stats(bnet_id)
   # Get categories
   categories = []
   doc.xpath('//*[@id="stats-section"]/div/select/option').each do |i|
+    #print i.text; puts
+    #print i.text.gsub("Lúcio","Lucio").gsub("Torbjörn","Torbjorn"); puts; puts
     categories.push(i.text)
   end
 
@@ -72,7 +83,39 @@ def get_stats(bnet_id)
       end
     end
   end
-  redis.set bnet_id, data.to_json
+  redis.set bnet_id, data.to_json.gsub("Lúcio","Lucio").gsub("Torbjörn","Torbjorn")
 end
 
-get_stats(battle_net_id(ARGV[0]))
+#
+def get_stats(bnet_id)
+  puts 'Running get_stats'
+  redis = $redis
+  if redis.get(bnet_id).nil?
+    return nil
+  end
+  return JSON.parse(redis.get(bnet_id))
+end
+
+# Dump entire database to JSON
+def data_dump()
+  puts 'Running data_dump'
+  redis = $redis
+  everyone = redis.keys
+  results = "{"
+  everyone.each do |bnet_id|
+    if bnet_id == everyone[(everyone.length - 1)]
+      results = results + "\"#{bnet_id}\":" + redis.get(bnet_id)+ "}"
+    else
+      results = results + "\"#{bnet_id}\":" + redis.get(bnet_id) + ","
+    end
+  end
+  return results
+end
+
+def in_database?(bnet_id)
+  print 'Running in_database? '
+  redis=$redis
+  users = redis.keys
+  puts users.include? bnet_id
+  return users.include? bnet_id
+end
---
+++ server.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+require './main.rb'
+
+set :port, 8080
+set :environment, :production
+
+get '/get_stats' do
+  content_type :json
+  bnet_id = battle_net_id(params[:id])
+  if params[:id].nil?
+    return data_dump()
+  end
+  if get_stats(bnet_id).nil?
+    crawl_stats(bnet_id)
+  end
+  get_stats(bnet_id).to_json
+end
+
+
+
+
+get '/get_stats/:hero' do
+  content_type :json
+  bnet_id = battle_net_id(params[:id])
+  puts bnet_id
+  if params[:id].nil?
+    return 'No User Called'
+  end
+
+  if !in_database?(bnet_id)
+    crawl_stats(bnet_id)
+  elsif !params[:stat].nil?
+      return get_stats(bnet_id)[params[:hero]][params[:stat]].to_json
+  else
+    return get_stats(bnet_id)[params[:hero]].to_json
+  end
+end
