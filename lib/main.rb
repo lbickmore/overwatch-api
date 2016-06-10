d168ec807b84ea3771db971b7cd6b9b86621497b
Logan Bickmore <loganbickmore91@gmail.com>
2016-06-10 00:28:19 -0700

    Class'ifying main.rb - In Progress

--- lib/main.rb
+++ lib/main.rb
@@ -8,8 +8,105 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 $redis = Redis.new(:password => 'foobared', :url => "redis://192.241.220.104")
 
+class BattleNetId
+  def initialize(input)
+    @bnet_id = input.strip.gsub('-','#').gsub(' ','')
+    p @bnet_id
+    if (@bnet_id.include? '#') && (!@bnet_id.split('#')[1].include?('psn')) && (!@bnet_id.split('#')[1].include?('xbl'))
+      # Check database for full bnet_id
+      if in_database?(@bnet_id)
+        return @bnet_id
+      end
+
+      # Check for PC user
+      id = @bnet_id.split('#')
+      page = HTTParty.get("https://playoverwatch.com/en-us/career/pc/us/#{id[0]}-#{id[1]}")
+      pc = Nokogiri::HTML(page)
+      if pc.inner_html.include? "Page Not Found"
+        @bnet_id = 'No User Found'
+      else
+        return @bnet_id
+      end
+    else
+      # Check for Playstation Network user
+      page = HTTParty.get("https://playoverwatch.com/en-us/career/psn/#{@bnet_id}")
+      psn = Nokogiri::HTML(page)
+      if !psn.inner_html.include? "Page Not Found"
+        if @bnet_id.include? '#psn'
+          return @bnet_id
+        else
+          @bnet_id = @bnet_id + '#psn'
+          return @bnet_id + '#psn'
+        end
+      end
+      if psn.inner_html.include? "Page Not Found"
+        # Check for Xbox Live user
+        page = HTTParty.get("https://playoverwatch.com/en-us/career/xbl/#{@bnet_id}")
+        xbl = Nokogiri::HTML(page)
+        if !xbl.inner_html.include? "Page Not Found"
+          if @bnet_id.include? '#xbl'
+            return @bnet_id
+          else
+            @bnet_id = @bnet_id + '#xbl'
+            return @bnet_id + '#xbl'
+          end
+        end
+      end
+      @bnet_id = 'No User Found'
+      #return 'No User Found'
+    end
+  end
+  def to_s
+    @bnet_id
+  end
+
+  def crawl_stats
+    print "#{@bnet_id} crawl_stats "
+    if @bnet_id == 'No User Found'
+      puts '- Failed (no user)'
+      return false
+    end
+
+    redis = $redis
+    bnet_arr = @bnet_id.split('#')
+    if !/\A\d+\z/.match(bnet_arr[1]) #if not a number
+      page = HTTParty.get("https://playoverwatch.com/en-us/career/#{bnet_arr[1]}/#{bnet_arr[0]}")
+    else
+      page = HTTParty.get("https://playoverwatch.com/en-us/career/pc/us/#{bnet_arr[0]}-#{bnet_arr[1]}")
+    end
+
+    doc = Nokogiri::HTML(page)
+
+    # Get categories
+    categories = []
+    doc.xpath('//*[@id="stats-section"]/div/select/option').each do |i|
+      categories.push(i.text)
+    end
+
+    # Create Hash structure for data
+    data = Hash.new()
+    categories.each do |category|
+      data[category]={}
+    end
+
+    # Crawl through data and add to structure!
+    categories.each_with_index do |category, i|
+      doc.xpath('//*[@id="stats-section"]/div/div').children[i].xpath('div/div/table/tbody').each do |item|
+        item.children.children.children.each_slice(2).to_a.each do |input|
+          data[category][input[0].text] = input[1].text
+        end
+      end
+    end
+    redis.set @bnet_id, data.to_json.gsub("Lúcio","Lucio").gsub("Torbjörn","Torbjorn")
+    puts '- Success'
+    return !redis[@bnet_id].empty?
+  end
+end
+## END class
+############
+
 def battle_net_id(bnet_id)
-  puts 'Running battle_net_id'
+  puts "Running battle_net_id"
 
   bnet_id = bnet_id.strip.gsub('-','#')
   if bnet_id.include? '#'
@@ -45,7 +142,7 @@ def battle_net_id(bnet_id)
     return 'No User Found'
   end
 end
-
+# End BNET ID
 
 def crawl_stats(bnet_id)
   puts 'Running crawl_stats'
@@ -114,7 +211,7 @@ end
 
 # Check if a bnet_id is in the database
 def in_database?(bnet_id)
-  print 'Running in_database? '
+  print "#{bnet_id} in_database? "
   redis=$redis
   users = redis.keys
   puts users.include? bnet_id
--- lib/tests.rb
+++ lib/tests.rb
@@ -3,6 +3,7 @@ require './main.rb'
 
 ## Tests for battle_net_id() ##
 # Standard Input Expectation
+=begin
 describe 'battle_net_id' do
   it 'Verifies Battlenet ID' do
     result = battle_net_id('BlueShoesYes-1548')
@@ -17,14 +18,39 @@ describe 'battle_net_id' do
     expect(result).to eq 'BlueShoesYes#1548'
   end
 end
-
+=end
 # Console Input
-describe 'battle_net_id' do
-  it 'Verifies Battlenet ID' do
-    result = battle_net_id('m_shad0w_06')
-    expect(result).to eq 'm_shad0w_06#psn'
+#describe 'battle_net_id' do
+#  it 'Verifies Battlenet ID' do
+#    result = battle_net_id('m_shad0w_06')
+#    expect(result).to eq 'm_shad0w_06#psn'
+#  end
+#end
+
+describe BattleNetId do
+  it '.initialize' do
+    bnet_id = BattleNetId.new('BlueShoesYes-1548').to_s
+    expect(bnet_id).to eq 'BlueShoesYes#1548'
+  end
+  it '.initialize' do
+    bnet_id = BattleNetId.new('m_shad0w_06').to_s
+    expect(bnet_id).to eq 'm_shad0w_06#psn'
+  end
+  it '.crawl_stats' do
+    bnet_id = BattleNetId.new('BlueShoesYes-1548')
+    expect(bnet_id.crawl_stats).to eq true
+  end
+
+  it '.crawl_stats' do
+    bnet_id = BattleNetId.new('m_shad0w_06#psn')
+    expect(bnet_id.crawl_stats).to eq true
+  end
+
+  it '.crawl_stats' do
+    bnet_id = BattleNetId.new('FakeNameInsertHere- LOL')
+
+    expect(bnet_id.crawl_stats).to eq false
   end
 end
-
 
 ## Tests for methodname() ##
